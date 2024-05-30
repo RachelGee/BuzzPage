1ee9c58bcbaf754a395ed564199ccbf5cb4c88f2
cadarius45 <cabrown676@gmail.com>
2024-05-30 12:21:21 -0500

    working on create route

--- Back-end-Buzzpage/controllers/posts.js
+++ Back-end-Buzzpage/controllers/posts.js
@@ -10,7 +10,17 @@ const router = express.Router();
 router.use(verifyToken);
 
 // create
-
+router.post('/posts', async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const post = await Post.create(req.body);
+        post._doc.author = req.user;
+        res.status(201).json(post);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+})
 // index
 
 // show by id
