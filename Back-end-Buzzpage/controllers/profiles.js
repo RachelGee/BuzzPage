3ff8345659634e09a48bd231e7be59488aeaa051
Debiddo <dzheng@terpmail.umd.edu>
2024-05-30 02:43:09 -0400

    created a user model and route for user sign in and out

---
+++ Back-end-Buzzpage/controllers/profiles.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const verifyToken = require('../middleware/verify-token');
+
+// return the user object if authorized else throw an error
+router.get('/:userId', verifyToken, async (req, res) => {
+    try {
+        // req.user is set using verifyToken
+        if (req.user._id !== req.params.userId){
+            return res.status(401).json({ error: "Unauthorized"})
+        }
+        const user = await User.findById(req.user._id);
+        if (!user) {
+            res.status(404)
+            throw new Error('Profile not found.');
+        }
+        res.json({ user });
+    } catch (error) {
+        if (res.statusCode === 404) {
+            res.status(404).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: error.message });
+        }
+    }
+});
+
+module.exports = router;
--- Back-end-Buzzpage/server.js
+++ Back-end-Buzzpage/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
 
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -12,8 +14,14 @@ mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/users', usersRouter);
+app.use('/profiles', profilesRouter);
+
 
 // Routes go here
 
