aebecc8f70afdc774e4735351de017724fe36ee4
cadarius45 <cabrown676@gmail.com>
2024-05-30 13:45:42 -0500

    added basic terms to post service, added imports to app.jsx and main.jsx. installed react-router-dom

---
+++ Front-end-Buzzpage/src/services/postService.js
@@ -0,0 +1,48 @@
+const BACKEND_URL = import.meta.env.VITE_BACK_END_SERVER_URL;
+
+// index to show all post "Hive Feed"
+const index = async () => {
+    try {
+      
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+//show a selected post
+const show = async (postId) => {
+    try {
+      
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+//create a new post 
+const create = async (postFormData) => {
+    try {
+     
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+//update a users post
+const update = async (postId, postFormData) => {
+    try {
+      
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+//delete the users post
+const deletePost = async (postId) => {
+    try {
+  
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+export { index, show, create, update , deletePost }; 
