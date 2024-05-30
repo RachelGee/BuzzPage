97ac532a05ae7c7c16a506b2eda53f14f398446a
Debiddo <dzheng@terpmail.umd.edu>
2024-05-30 02:46:51 -0400

    created a post and comment model

---
+++ Back-end-Buzzpage/models/post.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+    {
+      text: {
+        type: String,
+        required: true
+      },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    },
+    { timestamps: true }
+);
+
+const postSchema = new mongoose.Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      category: {
+        type: String,
+        required: true,
+        enum: ['News', 'Sports', 'Games', 'Movies', 'Music', 'Television'],
+      },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      comments: [commentSchema]
+    },
+    { timestamps: true }
+);
+  
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
+
