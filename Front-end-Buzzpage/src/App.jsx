0a9df21910fcc980378e9138e0c5a91d3cf0b0fa
Debiddo <dzheng@terpmail.umd.edu>
2024-05-31 11:29:03 -0400

    added pictures

--- Front-end-Buzzpage/src/App.jsx
+++ Front-end-Buzzpage/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 /*--------------------services--------------- */
-import * as authService from './services/authService'; 
+import * as authService from './services/authService';
 import * as postService from './services/postService';
 
 const App = () => {
