37ec78189e921b02f64479a24aa937c3bd012147
Debiddo <dzheng@terpmail.umd.edu>
2024-06-03 01:39:40 -0400

    added css to navbar

--- Front-end-Buzzpage/src/components/NavBar/NavBar.jsx
+++ Front-end-Buzzpage/src/components/NavBar/NavBar.jsx
@@ -1,28 +1,33 @@
 import Logo from '../../assets/images/logo.png'
 import { Link } from 'react-router-dom';
+import styles from "./NavBar.module.css"
 const NavBar = (props) => {
     return (
         <>
             {props.user ? (
-                <nav>
+                <nav className={styles.container}>
                     <Link className="logo-brandname" to='/'>
                         <img src={Logo} width={50} alt="" />
                         <div className="logo-title">BuzzPage</div>
                     </Link>
 
-                    <Link to='' onClick={props.handleSignout}>Sign Out</Link>
+                    <div className={styles.links}>
+                        <Link to={`/users/profile/${props.user._id}`} onClick={props.handleSignout}>Hello {props.user.username}</Link>
+                        <Link to='' onClick={props.handleSignout}>Sign Out</Link>
+                    </div>
                 </nav>
 
             ) : (
-                <nav>
+                <nav className={styles.container}>
                     <Link className="logo-brandname" to='/'>
                         <img src={Logo} width={50} alt="" />
                         <div className="logo-title">BuzzPage</div>
                     </Link>
 
-                    <Link to='users/signup'>Sign Up</Link>
-                    <br />
-                    <Link to='users/signin'>Login</Link>
+                    <div className={styles.links}>
+                        <Link to='users/signup'>Sign Up</Link>
+                        <Link to='users/signin'>Login</Link>
+                    </div>
                 </nav>
             )}
         </>
