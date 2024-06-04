40352704860aeb4f17f53b97dbb51666eb0bbd16
Debiddo <dzheng@terpmail.umd.edu>
2024-06-04 18:28:23 -0400

    Add basic style to sign up page

--- Front-end-Buzzpage/src/components/SignUpForm/SignUpForm.jsx
+++ Front-end-Buzzpage/src/components/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { signup } from '../../services/authService';
 import { useNavigate } from 'react-router-dom';
+import styles from './SignUpForm.module.css';
+import LoginSignup from '../../assets/images/LoginSignup.png';
+import Logo from '../../assets/images/logo.png'
 
 const SignUpForm = (props) => {
     const navigate = useNavigate();
@@ -11,16 +14,25 @@ const SignUpForm = (props) => {
         password: '',
         passwordConf: '',
     });
+    const [message, setMessage] = useState(['']);
+    const updateMessage = (msg) => {
+        setMessage(msg);
+    };
     const { firstName, lastName, username, password, passwordConf } = formData;
 
     const handleChange = (e) => {
+        updateMessage('');
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const newUser = await signup(formData);
-        navigate('/users/signin');
+        try {
+            const newUser = await signup(formData);
+            navigate('/users/signin');
+        } catch (err) {
+            updateMessage(err.message);
+        }
     };
 
     const isFormInvalid = () => {
@@ -29,60 +41,116 @@ const SignUpForm = (props) => {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <h1>Sign Up</h1>
-                <div>
-                    <label htmlFor="firstName">First Name:</label>
-                    <input
-                        type="text"
-                        id="firstName"
-                        value={firstName}
-                        name="firstName"
-                        onChange={handleChange}
-                    />
+            <div className={styles.container}>
+                <div className={styles.logoImage}>
+                    <p className='display-3'>What's all the BUZZ about?</p>
+                    <img src={Logo} alt="" />
+
                 </div>
-                <div>
-                    <label htmlFor="lastName">Last Name:</label>
-                    <input
-                        type="text"
-                        id="lastName"
-                        value={lastName}
-                        name="lastName"
-                        onChange={handleChange}
-                    />
+                <div className={styles.formContainer}>
+                    <form className={`border container-m p-5 h-75 ${styles.form}`} onSubmit={handleSubmit}>
+                        <img src={LoginSignup} className={styles.img} alt="" />
+                        <h1>Sign Up</h1>
+                        <div className={styles.signup}>
+                            <p>Have an account already?</p>
+                            <a href='' onClick={() => navigate('/users/signin')}>Login here</a>
+                        </div>
+                        <p>{message}</p>
+                        <div class="row">
+                            <div class="col">
+                                <label
+                                    htmlFor="firstName"
+                                    className="form-label"
+                                >
+                                    First Name
+                                </label>
+
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="firstName"
+                                    name="firstName"
+                                    value={firstName}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div class="col">
+                                <label
+                                    htmlFor="lastName"
+                                    className="form-label"
+                                >
+                                    Last Name
+                                </label>
+
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="lastName"
+                                    name="lastName"
+                                    value={lastName}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="mb-2">
+                            <label
+                                htmlFor="username"
+                                className="form-label"
+                            >
+                                Username
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="username"
+                                name="username"
+                                value={username}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="mb-2">
+                            <label
+                                htmlFor="password"
+                                className="form-label"
+                            >
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                name="password"
+                                className="form-control"
+                                id="password"
+                                value={password}
+                                onChange={handleChange}
+                                autoComplete='off'
+                            />
+
+                            <div className="form-text">We'll never share your password with anyone else.</div>
+                        </div>
+
+                        <div className="mb-3">
+                            <label
+                                htmlFor="confirm"
+                                className="form-label"
+                            >
+                                Confirm Password
+                            </label>
+                            <input
+                                type="password"
+                                name="passwordConf"
+                                className="form-control"
+                                id="password"
+                                value={passwordConf}
+                                onChange={handleChange}
+                                autoComplete='off'
+                            />
+                        </div>
+                        <button type="submit" disabled={isFormInvalid()} className={`btn btn-primary w-100 ${styles.button}`}>Sign Up</button>
+                    </form>
                 </div>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        value={username}
-                        name="username"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        name="password"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="confirm">Confirm Password:</label>
-                    <input
-                        type="password"
-                        id="confirm"
-                        value={passwordConf}
-                        name="passwordConf"
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit" disabled={isFormInvalid()}>Submit</button>
-            </form>
+            </div>
         </>
     );
 }
