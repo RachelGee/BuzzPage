cd2b58fc2a68da2cc27a489138ae2d4c32d99739
Debiddo <dzheng@terpmail.umd.edu>
2024-06-05 12:24:10 -0400

    added page transition

--- Front-end-Buzzpage/src/components/PostForm/PostForm.jsx
+++ Front-end-Buzzpage/src/components/PostForm/PostForm.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import * as postService from '../../services/postService'
+import PageTransition from '../PageTransition/PageTransition';
+
 
 const postForm = (props) => {
     const { userId } = useParams();
@@ -30,6 +32,7 @@ const postForm = (props) => {
     return (
 
         <>
+            <PageTransition />
             <form onSubmit={handleSubmit}>
                 <h1>{postId ? 'New Post' : 'New Post'}</h1>
 
