eb08878082d5870c16446b9d1531eb0c8732630b
cadarius45 <cabrown676@gmail.com>
2024-06-06 15:52:55 -0500

    having fun

--- Front-end-Buzzpage/src/components/UserPage/UserPage.jsx
+++ Front-end-Buzzpage/src/components/UserPage/UserPage.jsx
@@ -43,47 +43,51 @@ const UserPage = (props) => {
 
     return (  
         <main className={styles.container}>
-            <PageTransition />
+            {/* <PageTransition /> */}
             <div className={styles.sidebar}> 
                 <SideBar posts={user.posts} user={user}/>
             </div>
-                <div className={`card bg-dark m-auto mt-2 ${styles.userInfo}`}>
-                    <img src={user.image} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h1 className="card-text fw-bold fst-italic text-warning">{user.username}</h1>
-                    </div>
+            <div className={`card bg-dark m-auto mt-2 border-warning ${styles.userInfo}`}>
+                <img src={user.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h1 className="card-text fw-bold fst-italic text-warning">{user.username}</h1>
                 </div>
-                     <div className={`card  bg-dark m-auto text-warning mt-2 me-5 d-flex ${styles.userBio}`}>
-                         <h5 className="card-text">BEE: {user.firstName} {user.lastName}</h5>
-                         <p className="card-body">bio: {user.bio}</p>
-                         {/* for later development/ only allow user to edit or delete their own page */}
-                         {props.user._id !== userId ? (
-                            ''
-                        ) : (
-                        <>
-                            {!isOpen && (
-                                       <div className="row">
-                                       <div className="col">
-                                           <button className="btn btn-warning"><Link to={`/users/profile/${userId}/edit`} style={{ textDecoration: 'none', color: 'black' }}>Edit</Link></button>
-                                       </div>
-                                       <div className="col">
-                                           <button className={`btn btn-warning ${styles.delete}`} onClick={deleteMenu}>Delete</button>  
-                                       </div>
-                                   </div>  
-                                    )}
-                            {isOpen && (
-                                            <div className={`border container-m h-75 w-75 text-warning d-flex flex-column `} >are you sure
-                                                <button className="btn btn-warning" onClick={() => props.handleDeleteUser(userId)}>Delete</button>
-                                                <button  className={`btn-close ${styles.cancel}`} onClick={deleteMenu}/>
-                                        </div> 
-                                    )}
-                        </>
-                        )}
-                     </div>
+            </div>
+            <div className={`card d-flex  bg-dark m-auto text-warning mt-2 me-5 d-flex border-warning ${styles.userBio}`}>
+                <h5 className="card-text fs-2">Bee: {user.firstName} {user.lastName}</h5>
+                <p className="card-body text-center fs-4">{user.bio}</p>
+                {/* for later development/ only allow user to edit or delete their own page */}
+                {props.user._id !== userId ? ( ''
+                ) : (
+                <>
+                    {!isOpen && ( 
+                        <div className={`d-flex justify-content-center`}>
+                            <button className={`btn btn-warning `} onClick={deleteMenu}>Delete account</button>  
+                        </div> 
+                    )}
+                    {isOpen && (
+                        <div className={`container-m h-100 w-100 text-dark d-flex flex-column justify-content-center fs-1 ${styles.popup}`} >Are you sure you want to buzz away
+                            <div className='row '>
+                                <div className='col'>
+                                    <button className="btn btn-warning" onClick={() => props.handleDeleteUser(userId)}>Confirm</button>
+                                </div>
+                                <div className='col'>
+                                    <button  className={`btn btn-warning ${styles.cancel}`} onClick={deleteMenu}>Deny</button> 
+                                </div>
+                            </div>
+                        </div> 
+                    )}
+                </>
+                )}
+            </div>
             <div className={`d-flex  flex-wrap ${styles.userPost}`}>
-                {user.posts.length === 0 ? 'no posts': user.posts.map((post) =>(
+                {user.posts.length === 0 ? 'no posts'
+                
+                : 
+                
+                (user.posts.map((post) =>(
                     <Link to={`/posts/${post._id}`} style={{ textDecoration: 'none' }} key={post._id}><div >
-                        <div className={`card p-2 m-2 bg-dark  ${styles.postContainer} `}>
+                        <div className={`card p-2 m-2 bg-dark border-warning  ${styles.postContainer} `}>
                             <div>
                                 <h4 className="card-title text-warning">{post.title}</h4>
                                 <h6 className="card-header text-warning">{post.category}</h6>
@@ -92,7 +96,7 @@ const UserPage = (props) => {
                             {!post.image ? '': <img src={post.image} className={styles.image} alt='...'/>}
                         </div>
                     </div></Link>
-                    ))}
+                    )))}
                 </div>
         </main>
      );
