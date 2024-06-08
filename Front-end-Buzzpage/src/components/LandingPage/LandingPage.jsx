8a646e211a40568ab375dcd9e8c38dfcda1da49e
Debiddo <dzheng@terpmail.umd.edu>
2024-06-08 15:57:39 -0400

    add animation

--- Front-end-Buzzpage/src/components/LandingPage/LandingPage.jsx
+++ Front-end-Buzzpage/src/components/LandingPage/LandingPage.jsx
@@ -20,7 +20,6 @@ const LandingPage = () => {
                             </div>
                         </div>
 
-
                     </div>
                     <div className={`col-4 m-2 bg-dark text-light h-100 d-flex flex-column justify-content-around ${styles.boxC}`} style={{ minHeight: "70vh" }}>
                         <div className="row h-25">
@@ -28,7 +27,7 @@ const LandingPage = () => {
                         </div>
 
                         <div className="row h-25">
-                            <div className=" h-25"><img src={Logo} alt="" /></div>
+                            <div className=" h-25"><img className='img-fluid' src={Logo} alt="" /></div>
                         </div>
 
                         <div className="row h-25" >
@@ -38,7 +37,7 @@ const LandingPage = () => {
                         </div>
                     </div>
                     <div className={`col-2 d-flex align-items-center ${styles.boxR}`} style={{ backgroundColor: "#F4BE1E", minHeight: "60vh" }}>
-                        <p className='display-4'>Join the Buzz: Explore, Connect, Thrive!</p>
+                        <h1 className='h1'>Join the Buzz: Explore, Connect, Thrive!</h1>
                     </div>
                 </div>
 
